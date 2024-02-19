9152087d7ebe1695932a44f38b0985a9015c27ad
tai-kun <taikun.dev@gmail.com>
2024-02-19 09:00:14 +0900

    chore(theme/build): テーマのビルドツールに必要と考えられるスキーマを追加

---
+++ src/theme/build/schemas.ts
@@ -0,0 +1,121 @@
+import {
+  any,
+  type AnySchema,
+  array,
+  boolean,
+  custom,
+  enum_,
+  literal,
+  nullish,
+  number,
+  object,
+  type Output,
+  record,
+  string,
+  union,
+} from "valibot"
+import type { JsonValue } from "visit-json"
+import isJsonValue from "./isJsonValue"
+
+export enum PatchOperationMode {
+  Add = "add",
+  Merge = "merge",
+  Replace = "replace",
+  Copy = "copy",
+  Move = "move",
+  Remove = "remove",
+}
+
+export const PatchOperationModeSchema = enum_(PatchOperationMode)
+
+export const PatchPathLikeSchema = union([string(), array(string())])
+
+export const PatchOperationValueSchema: AnySchema<JsonValue> = any([
+  custom(value => isJsonValue(value)),
+])
+
+export const PatchOperationAddSchema = object({
+  mode: literal(PatchOperationMode.Add),
+  path: PatchPathLikeSchema,
+  value: PatchOperationValueSchema,
+  strict: nullish(boolean()),
+})
+
+export const PatchOperationMergeSchema = object({
+  mode: literal(PatchOperationMode.Merge),
+  path: PatchPathLikeSchema,
+  value: PatchOperationValueSchema,
+  strict: nullish(boolean()),
+})
+
+export const PatchOperationReplaceSchema = object({
+  mode: literal(PatchOperationMode.Replace),
+  path: PatchPathLikeSchema,
+  value: PatchOperationValueSchema,
+  strict: nullish(boolean()),
+})
+
+export const PatchOperationCopySchema = object({
+  mode: literal(PatchOperationMode.Copy),
+  path: PatchPathLikeSchema,
+  from: PatchPathLikeSchema,
+  strict: nullish(boolean()),
+})
+
+export const PatchOperationMoveSchema = object({
+  mode: literal(PatchOperationMode.Move),
+  path: PatchPathLikeSchema,
+  from: PatchPathLikeSchema,
+  strict: nullish(boolean()),
+})
+
+export const PatchOperationRemoveSchema = object({
+  mode: literal(PatchOperationMode.Remove),
+  path: PatchPathLikeSchema,
+  strict: nullish(boolean()),
+})
+
+export const PatchOperationSchema = union([
+  PatchOperationAddSchema,
+  PatchOperationMergeSchema,
+  PatchOperationReplaceSchema,
+  PatchOperationCopySchema,
+  PatchOperationMoveSchema,
+  PatchOperationRemoveSchema,
+])
+
+export const PatchMatrixSchema = record(union([string(), number()]))
+
+export const PatchSchema = object({
+  strict: nullish(boolean()),
+  matrix: nullish(PatchMatrixSchema),
+  operations: array(PatchOperationSchema),
+})
+
+export const ImportsSchema = record(string())
+
+export const PatchesSchema = array(PatchSchema)
+
+export const ExportsSchema = object({
+  include: array(string()),
+  exclude: array(string()),
+})
+
+export const BuildSchema = object({
+  imports: nullish(ImportsSchema),
+  patches: nullish(PatchesSchema),
+  exports: nullish(ExportsSchema),
+})
+
+if (cfgTest && cfgTest.url === import.meta.url) {
+  const { expectType } = await import("tsd")
+  const { describe, test } = cfgTest
+
+  describe("src/theme/build/schemas", () => {
+    test("PatchOperationSchema は PatchOperationMode のすべての値を実装している", () => {
+      expectType<Output<typeof PatchOperationSchema>["mode"]>(
+        {} as typeof PatchOperationMode[keyof typeof PatchOperationMode],
+      )
+    })
+  })
+}
