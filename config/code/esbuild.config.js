d31b7c492a51f7fb2d0f457f01264c97f0bc91fd
tai-kun <taikun.dev@gmail.com>
2024-02-22 09:11:30 +0900

    chore(config): tsconfig で moduleResolution: Bundler としつつも NodeNext と同様の効果を得るために ビルドの設定を改善

--- config/code/esbuild.config.js
+++ config/code/esbuild.config.js
@@ -2,6 +2,8 @@
 
 import { buildDefine } from "cfg-test/define"
 import { replace } from "esbuild-plugin-replace"
+import fs from "node:fs"
+import { join } from "node:path"
 
 /** @type {string[]} */
 const PURE = [
@@ -13,17 +15,22 @@ export default [
   {
     // General
 
-    format: "esm",
+    bundle: true,
     platform: "node",
 
     // Input
 
     entryPoints: [
-      "src/**/*.ts",
+      // "src/base/**/*.ts",
+      // "src/core/**/*.ts",
+      // "src/lab/**/*.ts",
+      // "src/icons/**/*.ts",
+      "src/utils/**/*.ts",
     ],
 
     // Output contents
 
+    format: "esm",
     lineLimit: 80,
 
     // Output location
@@ -46,12 +53,13 @@ export default [
       replace({
         __DEV__: "process.env.NODE_ENV !== \"production\"",
       }),
+      autoInsertExtenstion(),
     ],
   },
   {
     // General
 
-    format: "esm",
+    bundle: true,
     platform: "node",
 
     // Input
@@ -62,6 +70,7 @@ export default [
 
     // Output contents
 
+    format: "esm",
     lineLimit: 80,
 
     // Output location
@@ -91,6 +100,86 @@ export default [
       replace({
         __DEV__: "process.env.NODE_ENV !== \"production\"",
       }),
+      autoInsertExtenstion(),
     ],
   },
 ]
+
+/** @param {string} path */
+function isLocalFile(path) {
+  return path.startsWith(".")
+}
+
+/** @type {Record<string, boolean>} */
+const accessCache = {}
+
+/** @param {string} file */
+function readOk(file) {
+  if (file in accessCache) {
+    return accessCache[file]
+  }
+
+  try {
+    fs.accessSync(file, fs.constants.R_OK)
+
+    return accessCache[file] = true
+  } catch {
+    return accessCache[file] = false
+  }
+}
+
+/**
+ * @param {string} dir
+ * @param {string} file
+ */
+function getBuiltPath(dir, file) {
+  dir = join(dir, file)
+
+  for (const [src, dst] of Object.entries({ ".ts": ".js", ".tsx": ".jsx" })) {
+    if (readOk(dir + src)) {
+      return file + dst
+    }
+  }
+
+  return null
+}
+
+/** @returns {import("esbuild").Plugin} */
+function autoInsertExtenstion() {
+  return {
+    name: "auto-insert-extension",
+    setup(build) {
+      build.onResolve({ filter: /.*/ }, args => {
+        if (args.namespace !== "file" || args.kind !== "import-statement") {
+          return null
+        }
+
+        if (
+          args.resolveDir.includes("node_modules")
+          || !isLocalFile(args.path)
+        ) {
+          return {
+            external: true,
+          }
+        }
+
+        const builtPath = getBuiltPath(args.resolveDir, args.path)
+
+        if (builtPath) {
+          return {
+            path: builtPath,
+            external: true,
+          }
+        }
+
+        return {
+          errors: [
+            {
+              text: `File not found: ${args.path}`,
+            },
+          ],
+        }
+      })
+    },
+  }
+}
