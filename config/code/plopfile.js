feaacec079784e755f8085177c5642e81b78c063
tai-kun <taikun.dev@gmail.com>
2024-02-24 15:13:20 +0900

    chore: コンポーネントのテンプレートを追加 * forawrdRef 関数を追加 * base.css のレイヤーを修正

--- config/code/plopfile.js
+++ config/code/plopfile.js
@@ -13,8 +13,65 @@ export default plop => {
     path.normalize(path.dirname(arg))
       .replace(new RegExp(`${path.sep}{2,}`, "g"), "/")
       .replace(new RegExp(`${path.sep}*$`, "g"), ""))
+  plop.setHelper("kebab", arg =>
+    arg
+      .replace(/([a-z])([A-Z])/g, "$1-$2")
+      .toLowerCase())
+
+  /**
+   * @param {string} name
+   * @param {`/${string}`} group
+   * @param {`/${string}`} dirpath
+   */
+  const setComponentGenerator = (name, group, dirpath = group) => {
+    plop.setGenerator(name, {
+      description: "Add a component",
+      prompts: [
+        {
+          type: "input",
+          name: "name",
+          message: "Component name",
+        },
+      ],
+      actions: [
+        {
+          type: "add",
+          path: `../../src${dirpath}{{name}}/{{name}}.tsx`,
+          templateFile: "./templates/component/$name.tsx.hbs",
+          transform: template =>
+            template
+              .replaceAll("__group__", group)
+              .replaceAll("__dirname__", dirpath),
+        },
+        {
+          type: "add",
+          path: `../../src${dirpath}{{name}}/{{name}}.css`,
+          templateFile: "./templates/component/$name.css.hbs",
+        },
+        // {
+        //   type: "add",
+        //   path: `../../src${dirpath}{{name}}/{{name}}.spec.tsx`,
+        //   templateFile: "./templates/component/$name.spec.tsx.hbs",
+        // },
+        {
+          type: "add",
+          path: `../../src${dirpath}{{name}}/{{name}}.stories.tsx`,
+          templateFile: "./templates/component/$name.stories.tsx.hbs",
+          transform: template =>
+            template.replaceAll(
+              "__group__",
+              group.replace(/^\//, " ").trimEnd(),
+            ),
+        },
+      ],
+    })
+  }
 
   // Generators
+
+  setComponentGenerator("c", "/", "/core/")
+  setComponentGenerator("b", "/base/")
+  setComponentGenerator("l", "/lab/")
 
   plop.setGenerator("f", {
     description: "Add a function",
