ab7ca2b8a513908013819ff198004e9ee80c193e
tai-kun <taikun.dev@gmail.com>
2024-02-25 10:02:43 +0900

    chore(config): テスト時に css の import を消すように修正

--- config/cfg-test/replace.js
+++ config/cfg-test/replace.js
@@ -6,11 +6,17 @@ import { fileURLToPath } from "node:url"
 /** @type {import("node:module").LoadHook} */
 export async function load(url, _, next) {
   if (url.endsWith(".ts") || url.endsWith(".tsx")) {
+    let source = await fs.readFile(fileURLToPath(url), "utf8")
+    source = source.replace(/\b__DEV__\b/g, "true")
+
+    if (url.endsWith(".tsx") && source.includes(".css")) {
+      source = source.replace(/import\s+"[^"]+\.css"/g, "")
+    }
+
     return {
       // @ts-expect-error
       format: "ts",
-      source: (await fs.readFile(fileURLToPath(url), "utf8"))
-        .replaceAll("__DEV__", "true"),
+      source,
     }
   }
 
