75ccc2b15356052d192e92f9be8dbe4cf5c05d8e
tai-kun <taikun.dev@gmail.com>
2024-03-01 15:36:43 +0900

    chore(utils): useCss の実装を nano-css から独自実装に移行

--- src/utils/useCss.ts
+++ src/utils/useCss.ts
@@ -1,95 +1,15 @@
-import { create, type CssLikeObject } from "nano-css"
-import { addon as addonCache } from "nano-css/addon/cache.js"
-import { addon as addonRule } from "nano-css/addon/rule.js"
 import * as React from "react"
-import type {
-  JsonArrayLike,
-  JsonObjectLike,
-  JsonPrimitiveLike,
-  JsonValueLike,
-} from "visit-json"
+import createCssRenderer, { type CssObject } from "./createCssRenderer"
 import onceCell from "./singleton"
 
-// https://github.com/fastify/fast-json-stringify/blob/14338e22fdb39cf12378355d26fa281ec3d636a5/lib/serializer.js#L4
-const JSON_STR_ESC_REGEX =
-  /[\u0000-\u001f\u0022\u005c\ud800-\udfff]|[\ud800-\udbff](?![\udc00-\udfff])|(?:[^\ud800-\udbff]|^)[\udc00-\udfff]/
+export type {
+  CssArray,
+  CssObject,
+  CssPrimitive,
+  CssValue,
+} from "./createCssRenderer"
 
-const UPPER_CASE_REGEX = /[A-Z]/g
-
-/**
- * JSON の値を文字列に変換する。
- *
- * @param value JSON の値。
- * @returns 文字列。
- */
-function stringifyValue(value: JsonValueLike): string {
-  return Array.isArray(value)
-    ? stringifyArray(value)
-    : typeof value === "object" && value !== null
-    ? stringifyObject(value)
-    : stringifyPrimitive(value)
-}
-
-/**
- * JSON の配列を文字列に変換する。
- *
- * @param array JSON の配列。
- * @returns 文字列。
- */
-function stringifyArray(array: JsonArrayLike): string {
-  return "[" + array.map(stringifyValue) + "]"
-}
-
-/**
- * JSON のオブジェクトを文字列に変換する。
- *
- * @param object JSON のオブジェクト。
- * @returns 文字列。
- */
-function stringifyObject(object: JsonObjectLike): string {
-  let str = "{"
-
-  for (const [key, value] of Object.entries(object)) {
-    str += `${stringifyPrimitive(key)}:${stringifyValue(value)},`
-  }
-
-  return str + "}"
-}
-
-/**
- * JSON のプリミティブを文字列に変換する。
- *
- * @param primitive JSON のプリミティブ。
- * @returns 文字列。
- */
-function stringifyPrimitive(primitive: JsonPrimitiveLike): string {
-  if (typeof primitive !== "string") {
-    return String(primitive ?? null)
-  }
-
-  if (JSON_STR_ESC_REGEX.test(primitive)) {
-    return JSON.stringify(primitive)
-  }
-
-  return `"${primitive}"`
-}
-
-export const renderer = onceCell("nano-css", () => {
-  const nano = create({
-    pfx: "sui",
-    kebab(prop: string) {
-      return prop.charCodeAt(0) === 45 /* "-" */
-        ? prop
-        : prop.replace(UPPER_CASE_REGEX, "-$&").toLowerCase()
-    },
-    stringify: stringifyObject,
-  } as {})
-
-  addonCache(nano)
-  addonRule(nano)
-
-  return nano
-})
+export const renderer = onceCell("css-renderer", createCssRenderer)
 
 /**
  * スタイルルールが適用された CSS のクラス名を返す。
@@ -102,7 +22,7 @@ export const renderer = onceCell("nano-css", () => {
  */
 export default function useCss<A extends readonly unknown[]>(
   name: string,
-  make: (...args: A) => CssLikeObject | null | undefined,
+  make: (...args: A) => CssObject | null | undefined,
   args: A,
 ): string {
   if (__DEV__) {
@@ -133,11 +53,19 @@ export default function useCss<A extends readonly unknown[]>(
     () => {
       const style = make(...args)
 
-      return style == null
-        ? ""
-        : renderer
-          .rule!({ [`@layer sui.components.${name}.dynamic`]: style })
-          .substring(1 /* " ".length */)
+      if (style == null) {
+        return ""
+      }
+
+      const { hash } = renderer.put(hash => ({
+        [`@layer sui.components.${name}.dynamic`]: {
+          [`.sui-${hash}`]: style,
+        },
+      }))
+
+      return hash
+        ? `sui-${hash}`
+        : ""
     },
     args,
   )
@@ -156,16 +84,18 @@ if (cfgTest && cfgTest.url === import.meta.url) {
       using renderResult = renderHook(() => useCss("box", makeStyle, ["red"]))
       const { result } = renderResult
 
-      assert.match(result.current, /^sui_[0-9a-z]+$/)
-      assert.equal(
-        normalizeCss(renderer.raw),
-        normalizeCss(`
-          @layer sui.components.box.dynamic {
-            .${result.current} {
-              color: red;
+      assert.match(result.current, /^sui-[0-9a-z]+$/)
+      assert.deepEqual(
+        renderer.raw.map(normalizeCss),
+        [
+          `
+            @layer sui.components.box.dynamic {
+              .${result.current} {
+                color: red;
+              }
             }
-          }
-        `),
+          `,
+        ].map(normalizeCss),
       )
     })
   })
