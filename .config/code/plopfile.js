4144b5196c7c1d0aaa7c400691d53a001caeaf24
tai-kun <taikun.dev@gmail.com>
2024-05-14 11:02:20 +0900

    init

---
+++ .config/code/plopfile.js
@@ -0,0 +1,98 @@
+// @ts-check
+
+/**
+ * @param {import("plop").NodePlopAPI} plop
+ */
+export default plop => {
+  plop.setGenerator("p", {
+    description: "パッケージを追加する",
+    prompts: [
+      {
+        type: "name",
+        name: "name",
+        message: "パッケージ名",
+        validate: input =>
+          (typeof input === "string" && /^[a-z0-9-][a-z0-9-._]*$/.test(input))
+          || "パッケージ名が不正です",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "../../packages/{{name}}/package.json",
+        templateFile: "./templates/package/package.json.hbs",
+      },
+      {
+        type: "add",
+        path: "../../packages/{{name}}/tsconfig.json",
+        templateFile: "./templates/package/tsconfig.json.hbs",
+      },
+      {
+        type: "add",
+        path: "../../packages/{{name}}/.config/cfg-test.json",
+        templateFile: "./templates/package/.config/cfg-test.json.hbs",
+      },
+      {
+        type: "add",
+        path: "../../packages/{{name}}/.config/build/esbuild.js",
+        templateFile: "./templates/package/.config/build/esbuild.js.hbs",
+      },
+      {
+        type: "add",
+        path: "../../packages/{{name}}/.config/build/tsconfig.build.json",
+        templateFile:
+          "./templates/package/.config/build/tsconfig.build.json.hbs",
+      },
+      {
+        type: "add",
+        path: "../../packages/{{name}}/.config/code/plopfile.js",
+        templateFile: "./templates/package/.config/code/plopfile.js.hbs",
+        transform: template =>
+          template
+            .replaceAll("__dirname_outpath__", "{{dirname outpath}}")
+            .replaceAll("__basename_outpath__", "{{basename outpath}}"),
+      },
+      {
+        type: "add",
+        path:
+          "../../packages/{{name}}/.config/code/templates/function/$name.ts.hbs",
+        templateFile:
+          "./templates/package/.config/code/templates/function/$name.ts.hbs",
+        transform: template =>
+          template
+            .replaceAll("__dirname_outpath__", "{{dirname outpath}}")
+            .replaceAll("__basename_outpath__", "{{basename outpath}}"),
+      },
+      {
+        type: "add",
+        path: "../../packages/{{name}}/scripts/test",
+        templateFile: "./templates/package/scripts/test.hbs",
+      },
+      {
+        type: "add",
+        path: "../../packages/{{name}}/scripts/build",
+        templateFile: "./templates/package/scripts/build.hbs",
+      },
+      {
+        type: "add",
+        path: "../../packages/{{name}}/scripts/check",
+        templateFile: "./templates/package/scripts/check.hbs",
+      },
+      {
+        type: "add",
+        path: "../../packages/{{name}}/scripts/commit-msg",
+        templateFile: "./templates/package/scripts/commit-msg.hbs",
+      },
+      {
+        type: "add",
+        path: "../../packages/{{name}}/src/env.d.ts",
+        templateFile: "./templates/package/src/env.d.ts.hbs",
+      },
+      {
+        type: "add",
+        path: "../../packages/{{name}}/.vscode/settings.json",
+        templateFile: "../../.vscode/settings.json",
+      },
+    ],
+  });
+};
---
+++ .config/code/templates/package/.config/build/esbuild.js.hbs
@@ -0,0 +1,5 @@
+// @ts-check
+
+import { build } from "@sui/build";
+
+await build();
